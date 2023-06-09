ade48435e0b5e266f8857bd05ad116461d61d8ec
Edward Kelder Jostell <Edward.Kelder.Jostell@skandia.se>
2023-06-09 17:19:17 +0200

    New way of not having duplicates in pokedex

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   //Engine State
   const [allPokemons, setAllPokemons] = useState<any>([]);
   const [loadPoke, setLoadPoke] = useState(
-    "https://pokeapi.co/api/v2/pokemon?limit=200"
+    "https://pokeapi.co/api/v2/pokemon?limit=10"
   );
   const [collectedPokemons, setCollectedPokemons] = useState<any>([]);
   const [cardArray, setCardArray] = useState<any>([]);
@@ -54,28 +54,16 @@ function App() {
     }
   }, [cardArray]);
 
-  const hasSameId = (arr: any[]) => {
-    const ids = new Set(arr.map((obj) => obj.id));
-
-    return ids.size !== arr.length;
-  };
-
-  useEffect(() => {
-    const containSameId = hasSameId(collectedPokemons);
-    if (containSameId) {
-      const seen = new Set();
-      setCollectedPokemons(
-        collectedPokemons.filter((item: any) => {
-          const duplicate = seen.has(item.id);
-          seen.add(item.id);
-          return !duplicate;
-        })
-      );
-    }
-  }, [collectedPokemons]);
-
   const handleCollectPokemon = (poke: any, index: number) => {
-    setCollectedPokemons((currentPokemons: any) => [...currentPokemons, poke]);
+    const isDuplicate = collectedPokemons.find((x: any) => {
+      return x.id === poke.id;
+    });
+    if (!isDuplicate) {
+      setCollectedPokemons((currentPokemons: any) => [
+        ...currentPokemons,
+        poke,
+      ]);
+    }
     setCardArray(cardArray.filter((value: any, i: any) => i !== index));
   };
 
